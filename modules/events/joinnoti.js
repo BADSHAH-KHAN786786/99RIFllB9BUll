702ac80dd706f561fc304f100bd5a82b9defc25e
GITHUB78600 <githup3@yandex.com>
2024-09-05 20:45:24 +0530

    Update joinnoti.js

--- modules/events/joinnoti.js
+++ modules/events/joinnoti.js
@@ -48,7 +48,7 @@ memLength.sort((a, b) => a - b);
                         .replace(/\{threadName}/g, threadName);                        
 
       var link = [
-"https://i.postimg.cc/T20CvhtW/ezgif-2-c305128791.gif",
+"https://i.postimg.cc/fyWYDk8H/b4995090b30f9dec8aefa98003aca288.gif",
       ];
                                 var callback = () => api.sendMessage({ body: msg, attachment: fs.createReadStream(__dirname + "/cache/leiamnashJ.jpg"), mentions }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/leiamnashJ.jpg"));
     return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname + "/cache/leiamnashJ.jpg")).on("close", () => callback());       
